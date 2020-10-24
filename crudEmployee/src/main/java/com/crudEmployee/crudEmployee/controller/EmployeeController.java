a2fa7ee7ffa48465010b4f8c0ec546c4ef5667b8
Frank Uzoka <Frankuzoka@hotmail.com>
2020-10-24 18:27:43 -0400

    Creating List Employee REST API

---
+++ crudEmployee/src/main/java/com/crudEmployee/crudEmployee/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.crudEmployee.crudEmployee.controller;
+
+import com.crudEmployee.crudEmployee.model.Employee;
+import com.crudEmployee.crudEmployee.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployee(){
+
+        return employeeRepo.findAll();
+    }
+
+}
