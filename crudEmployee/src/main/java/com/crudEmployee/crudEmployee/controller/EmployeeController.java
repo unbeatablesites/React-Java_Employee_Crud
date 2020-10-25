c6b2946428334c32bb5eadec51963809f86270c0
Frank Uzoka <Frankuzoka@hotmail.com>
2020-10-25 15:37:07 -0400

    Creating Get Employee By Id REST API

--- crudEmployee/src/main/java/com/crudEmployee/crudEmployee/controller/EmployeeController.java
+++ crudEmployee/src/main/java/com/crudEmployee/crudEmployee/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.crudEmployee.crudEmployee.controller;
 
+import com.crudEmployee.crudEmployee.exception.ResourceNotFound;
 import com.crudEmployee.crudEmployee.model.Employee;
 import com.crudEmployee.crudEmployee.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,12 +23,19 @@ public class EmployeeController {
     }
 
     // create employee rest API
-    @PostMapping("employees")
+    @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
 
         return employeeRepo.save(employee);
 
     }
+    // Get employee by id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
 
+        Employee employee = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFound("Employee not found with id : " + id));
+
+        return ResponseEntity.ok(employee);
+    }
 
 }
