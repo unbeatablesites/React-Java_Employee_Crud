6ecd497ecb046c10b5fc47d4395b9b35482480d0
Frank Uzoka <Frankuzoka@hotmail.com>
2020-10-24 22:40:02 -0400

    Add Header and Footer to React App

--- react-frontend-employee-crud/src/App.js
+++ react-frontend-employee-crud/src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-    <div className="Container">
-      <ListEmployeeComponent />
+    <div>
+      <HeaderComponent />
+      <div className="Container">
+        <ListEmployeeComponent />
+      </div>
+      <FooterComponent />
     </div>
   );
 }
