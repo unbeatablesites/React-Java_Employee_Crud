73af7d85f6b1ecd909d2759b2442156229999833
Frank Uzoka <Frankuzoka@hotmail.com>
2020-10-25 01:30:12 -0400

    Creating Add Employee REST API

--- crudEmployee/src/main/java/com/crudEmployee/crudEmployee/controller/EmployeeController.java
+++ crudEmployee/src/main/java/com/crudEmployee/crudEmployee/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.crudEmployee.crudEmployee.controller;
 import com.crudEmployee.crudEmployee.model.Employee;
 import com.crudEmployee.crudEmployee.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
@@ -22,5 +19,14 @@ public class EmployeeController {
 
         return employeeRepo.findAll();
     }
+
+    // create employee rest API
+    @PostMapping("employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+
+        return employeeRepo.save(employee);
+
+    }
+
 
 }
